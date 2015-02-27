4f184d10d3527b0fb5fd50763c213e9dee44b3b6
Jorge Peres <jorgepgjr@hotmail.com>
2015-02-27 16:20:02 -0300

    funfou

--- src/main/java/fergunboto/Start.java
+++ src/main/java/fergunboto/Start.java
@@ -5,22 +5,17 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
 import nu.pattern.OpenCV;
 
 import org.opencv.core.Core;
 import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -31,11 +26,11 @@ public class Start {
 
 	public static void main(String[] args) throws Exception {
 		OpenCV.loadShared();
-//		System.out.println("Welcome to OpenCV " + Core.VERSION);
-//		System.out.println("Welcome to OpenCV " + Core.NATIVE_LIBRARY_NAME);
+		// System.out.println("Welcome to OpenCV " + Core.VERSION);
+		// System.out.println("Welcome to OpenCV " + Core.NATIVE_LIBRARY_NAME);
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//		Mat m = Mat.eye(3, 3, CvType.CV_8UC1);
-//		System.out.println("m = " + m.dump());
+		// Mat m = Mat.eye(3, 3, CvType.CV_8UC1);
+		// System.out.println("m = " + m.dump());
 		Start.captureScreen();
 		matchImage();
 	}
@@ -43,15 +38,15 @@ public class Start {
 	public static void matchImage() throws Exception {
 		System.out.println("\nRunning Template Matching");
 
-		Mat img = Highgui.imread("src/main/resources/poker.jpg");
-		Mat templ = Highgui.imread("src/main/resources/template_pot.jpg");
+		Mat img = Highgui.imread("src/main/resources/mesa.tif", CvType.CV_32FC1);
+		Mat templ = Highgui.imread("src/main/resources/pot_template.tif");
 
 		// / Create the result matrix
 		int result_cols = img.cols() - templ.cols() + 1;
 		int result_rows = img.rows() - templ.rows() + 1;
 		Mat result = new Mat(result_rows, result_cols, CvType.CV_32FC1);
 
-		/// Do the Matching and Normalize
+		// / Do the Matching and Normalize
 		Imgproc.matchTemplate(img, templ, result, Imgproc.TM_CCOEFF);
 		Core.normalize(result, result, 0, 1, Core.NORM_MINMAX, -1, new Mat());
 
@@ -71,39 +66,41 @@ public class Start {
 		Point pontoFinal = new Point(matchLoc.x + templ.cols(), matchLoc.y
 				+ templ.rows());
 
-		//Rect da label pot
+		// Rect da label pot
 //		Core.rectangle(img, matchLoc, pontoFinal, new Scalar(0, 255, 0));
 
-		Point pontoDoValor = new Point(pontoFinal.x + 80, pontoFinal.y - 25);
-
-		//Rect do valor do pot
-		Rect valor = new Rect(pontoFinal, pontoDoValor); 
-//		Core.rectangle(img, pontoFinal, pontoDoValor, new Scalar(255, 0, 0));
-		 
-		Mat x = img.submat(valor);
+		Point pontoFinalDoValor = new Point(pontoFinal.x + 20, pontoFinal.y - 25);
+		Point pontoInicialDoValor = new Point(pontoFinal.x, pontoFinal.y - 6);
 		
+		// Rect do valor do pot
+		Rect valor = new Rect(pontoInicialDoValor, pontoFinalDoValor);
+//		Core.rectangle(img, pontoFinal, pontoFinalDoValor, new Scalar(255, 0, 0));
+
+		Mat x = img.submat(valor);
+
 		// Save the visualized detection.
 		System.out.println("Writing " + "saida.jpg");
 		Highgui.imwrite("src/main/resources/saida.jpg", img);
 
-		Mat grayScale = x.clone();
-		Mat grayScale2 = new Mat();
+		 Mat grayScale = x.clone();
+		 Mat grayScale2 = new Mat();
 		
-		Imgproc.cvtColor(x, grayScale , Imgproc.COLOR_RGB2GRAY);
-		Imgproc.threshold(grayScale, grayScale2, 127, 255, 0);
-		
-		Highgui.imwrite("src/main/resources/valorpot.jpg", grayScale2);
-		
-		File imagemValorPot = new File("src/main/resources/valorpot.jpg");
-//		MatOfByte bytemat = new MatOfByte();
-//		Highgui.imencode(".jpg", x, bytemat);
-//		byte[] bytes = bytemat.toArray();
-//		InputStream in = new ByteArrayInputStream(bytes);
-//		BufferedImage buffImg = ImageIO.read(in);
-		try{
+		 Imgproc.cvtColor(x, grayScale , Imgproc.COLOR_RGB2GRAY);
+		 Imgproc.threshold(grayScale, grayScale2, 127, 255, 0);
+		 
+		 Highgui.imwrite("src/main/resources/valorpot.bmp", x);
+
+		File imagemValorPot = new File("src/main/resources/valorpot.bmp");
+		// MatOfByte bytemat = new MatOfByte();
+		// Highgui.imencode(".jpg", x, bytemat);
+		// byte[] bytes = bytemat.toArray();
+		// InputStream in = new ByteArrayInputStream(bytes);
+		// BufferedImage buffImg = ImageIO.read(in);
+		try {
 			Runtime rt = Runtime.getRuntime();
-			Process pr = rt.exec("tesseract src/main/resources/valorpot.jpg src/main/resources/valorpot");		
-		} catch (Exception e){
+			Process pr = rt
+					.exec("tesseract src/main/resources/valorpot.bmp src/main/resources/valorpot contrast negate digits");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
